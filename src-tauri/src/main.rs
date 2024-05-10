631c462831201b9be8a60ea7f8fa6c77f5f76f38
sehnryr <youn@melois.dev>
2024-05-10 18:38:50 +0200

    style: format rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,12 @@
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
