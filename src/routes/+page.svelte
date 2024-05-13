82604b47b7ab18f93291e51e779c8e01942e533d
sehnryr <youn@melois.dev>
2024-05-13 03:48:27 +0200

    feat: add title, settings button + page
    change placeholder images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import MangaGrid from '$lib/components/MangaGrid.svelte';
     import MangaTile from '$lib/components/MangaTile.svelte';
     import Search from '$lib/components/Search.svelte';
+    import { Settings } from 'lucide-svelte';
 
     /** @type {Array<{ id: number, title: string, cover_src: string, unread_chapters: number }>} */
     let mangaList = [];
@@ -10,14 +11,20 @@
     for (let i = 0; i < 20; i++) {
         mangaList.push({
             id: i,
-            title: `Manga Donec eu finibus dui, vitae vulputate lorem. Sed et vestibulum nulla, quis pellentesque massa.`,
-            cover_src: 'https://via.placeholder.com/150',
+            title: `Donec eu finibus dui, vitae vulputate lorem. Sed et vestibulum nulla, quis pellentesque massa.`,
+            cover_src: 'https://source.unsplash.com/random/300x400/?img=' + i,
             unread_chapters: Math.floor(Math.random() * 100) * (i % 2)
         });
     }
 </script>
 
 <div>
+    <section>
+        <a href="/settings">
+            <Settings />
+        </a>
+    </section>
+    <h1>Library</h1>
     <Search placeholder="Search for manga..." />
     <MangaGrid>
         {#each mangaList as manga}
@@ -30,7 +37,16 @@
     div {
         display: flex;
         flex-direction: column;
-        gap: var(--size-2);
-        padding: var(--size-2);
+        gap: var(--size-3);
+        padding: var(--size-3);
+    }
+
+    h1 {
+        font-size: var(--font-size-7);
+    }
+
+    section {
+        display: flex;
+        justify-content: flex-end;
     }
 </style>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { ArrowLeft } from "lucide-svelte";
+</script>
+
+<a href="/">
+    <ArrowLeft />
+</a>
+<h1>Settings</h1>
+
+<style>
+    h1 {
+        font-size: var(--font-size-7);
+    }
+</style>
