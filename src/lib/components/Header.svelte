c404933aee9dfefed818a112d010a4cd79fcdfb6
sehnryr <youn@melois.dev>
2024-05-27 16:03:17 +0200

    fix: header height is now fixed

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,5 @@
-<header class="flex flex-row items-center gap-6 bg-accent px-3 py-4">
-    <slot />
+<header class="h-16 bg-accent px-3 py-4">
+    <div class="flex h-full flex-row items-center gap-6">
+        <slot />
+    </div>
 </header>
