30c31e4ada7ac4bb7c75b19b4a96143d78bc4da3
sehnryr <youn@melois.dev>
2024-05-27 16:02:53 +0200

    feat: fix manga card design

--- src/lib/components/MangaTile.svelte
+++ src/lib/components/MangaTile.svelte
@@ -6,20 +6,17 @@
 </script>
 
 <a href="/manga?id={encodeURIComponent(manga.id)}" class="relative">
-    <AspectRatio
-        ratio={3 / 4}
-        class="m-1 after:absolute after:left-0 after:top-0 after:h-full after:w-full
-        after:rounded-md after:bg-gradient-to-b after:from-transparent after:from-60%
-        after:to-black/80 after:content-['']"
-    >
-        <img
-            src={manga.cover_src}
-            alt="{manga.title} cover"
-            loading="lazy"
-            class="rounded-md object-cover"
-        />
-    </AspectRatio>
-    <h1 class="absolute bottom-0 left-0 m-3 line-clamp-2 font-semibold text-white">
+    <div class="mx-1 mt-1">
+        <AspectRatio ratio={3 / 4}>
+            <img
+                src={manga.cover_src}
+                alt="{manga.title} cover"
+                loading="lazy"
+                class="rounded-md object-cover"
+            />
+        </AspectRatio>
+    </div>
+    <h1 class="mx-1 line-clamp-2 h-10 text-sm font-semibold text-foreground">
         {manga.title}
     </h1>
     {#if manga.unread_chapters > 0}
