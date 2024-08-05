a9ba20eaea5957e052645104ad43bb6827b08134
sehnryr <youn@melois.dev>
2024-08-05 23:52:35 +0200

    feat: remove daisyui for shadcn/ui's theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,48 @@ module.exports = {
     mode: "jit",
     content: ["*.html", "./src-ui/src/**/*.rs"],
     theme: {
-        extend: {},
+        extend: {
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            colors: {
+                accent: {
+                    DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+                    foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
+                },
+                background: "hsl(var(--background) / <alpha-value>)",
+                border: "hsl(var(--border) / <alpha-value>)",
+                card: {
+                    DEFAULT: "hsl(var(--card) / <alpha-value>)",
+                    foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+                    foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+                },
+                foreground: "hsl(var(--foreground) / <alpha-value>)",
+                input: "hsl(var(--input) / <alpha-value>)",
+                muted: {
+                    DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+                    foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+                    foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
+                },
+                primary: {
+                    DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+                    foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+                },
+                ring: "hsl(var(--ring) / <alpha-value>)",
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+                    foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+                },
+            },
+        },
     },
-    plugins: [require("daisyui")],
+    plugins: [],
 };
