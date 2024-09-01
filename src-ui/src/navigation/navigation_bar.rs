9994dbe9ed4f53bc88a3de07e2b24a68b016b75f
sehnryr <youn@melois.dev>
2024-09-02 01:21:55 +0200

    feat: add icons to navigation bar

--- src-ui/src/navigation/navigation_bar.rs
+++ src-ui/src/navigation/navigation_bar.rs
@@ -1,34 +1,36 @@
 use leptos::*;
+use leptos_remix_icon::Icon;
 use leptos_router::*;
 
 #[component]
 pub fn NavigationBar() -> impl IntoView {
     view! {
         <nav class="flex flex-row items-center justify-around bg-muted px-3 py-4">
-            <NavigationElement href="/" text="Library"/>
-            <NavigationElement href="/browse" text="Browse"/>
-            <NavigationElement href="/more" text="More"/>
+            <NavigationElement href="/" icon="book-marked-line" text="Library"/>
+            <NavigationElement href="/browse" icon="compass-3-line"  text="Browse"/>
+            <NavigationElement href="/more" icon="more-line" text="More"/>
         </nav>
     }
 }
 
 #[component]
 pub fn NavigationElement(
-    #[prop(into, default = "#".to_string())] href: String,
-    #[prop(into)] text: String,
+    href: &'static str,
+    icon: &'static str,
+    text: &'static str,
 ) -> impl IntoView {
-    let href_clone = href.clone();
-
     let class = move || {
-        if href_clone == use_location().pathname.get() {
-            "flex flex-col items-center gap-1"
-        } else {
-            "flex flex-col items-center gap-1 text-muted-foreground"
-        }
+        format!(
+            "flex flex-col items-center gap-1 {}",
+            href.ne(&use_location().pathname.get())
+                .then_some("text-muted-foreground")
+                .unwrap_or_default()
+        )
     };
 
     view! {
         <A href={href} class={class}>
+            <Icon icon={icon} class="text-2xl"/>
             <span>{text}</span>
         </A>
     }
