3c0e2fb136849c0f5cdaa30dd1e60f6a76f50f7a
Breval <breval2000@live.fr>
2024-11-07 02:24:56 +0100

    feat: Update Browse component rendering logic
    Refactor Browse component to use new service functions for fetching extensions and icon URLs. Simplified useEffect hook for better readability and maintainability.

--- src-www/pages/Browse/Browse.tsx
+++ src-www/pages/Browse/Browse.tsx
@@ -1,58 +1,56 @@
-import { invoke } from "@tauri-apps/api/core";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
-import { Extension, Source } from "../../types/extension";
-import { Link } from "react-router-dom";
+import {Extension} from "../../types/extension.ts";
+import {getIconUrl} from "../../services/extensions.service.ts";
+import {getExtensions} from "../../tauri.ts";
+import {Link} from "react-router-dom";
 
 export default function Browse() {
-  const [extensions, setExtensions] = useState<Array<Extension>>([]);
+    const [extensions, setExtensions] = useState<Extension[]>([]);
 
-  useEffect(() => {
-    invoke<Array<[string, Source, string]>>("get_extensions").then((data) => {
-      setExtensions(data.map(([id, source, iconPath]) => {
-        return new Extension(id, source, iconPath);
-      }));
-    });
-  }, []);
+    useEffect(() => {
+        getExtensions().then(setExtensions);
+    }, []);
 
-  return (
-    <ul
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 8,
-        padding: "0 2rem",
-      }}
-    >
-      {extensions.map((extension: Extension) => {
-        return (
-          <li
-            key={extension.id}
-            style={{ display: "flex", alignItems: "center", gap: 12 }}
-          >
-            <img src={extension.iconUrl} style={{ width: 48, height: 48 }} />
-            <div>
-              <span style={{ display: "block", fontSize: 16 }}>
-                {extension.name}
-              </span>
-              <div style={{ opacity: 0.7, fontSize: 14 }}>
-                {extension.language}
-              </div>
-            </div>
-            <Link
-              to={{
-                pathname: "/browse/" + extension.id,
-              }}
-              state={{ extension: extension }}
-              style={{
-                marginLeft: "auto",
-              }}
-            >
-              Browse
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+
+    return (
+        <ul
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 8,
+                padding: "0 2rem",
+            }}
+        >
+            {extensions.map((extension: Extension) => {
+                return (
+                    <li
+                        key={extension.id}
+                        style={{display: "flex", alignItems: "center", gap: 12}}
+                    >
+                        <img
+                            src={getIconUrl(extension.iconPath)}
+                            style={{width: 48, height: 48}}
+                        />
+                        <div>
+          <span style={{display: "block", fontSize: 16}}>
+            {extension.source.name}
+          </span>
+                            <div style={{opacity: 0.7, fontSize: 14}}>
+                                {extension.source.language}
+                            </div>
+                        </div>
+                        <Link
+                            to={`/browse/${extension.id}`}
+                            style={{
+                                marginLeft: "auto",
+                            }}
+                        >
+                            Browse
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
