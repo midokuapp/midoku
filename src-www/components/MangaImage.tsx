397a138d694ec4a9b2eb2652ea29d53a46b7f753
Breval <breval2000@live.fr>
2024-11-07 03:43:28 +0100

    feat: Improve fallback image handling
    Refactored the MangaImage component to handle fallback image source more efficiently.

--- src-www/components/MangaImage.tsx
+++ src-www/components/MangaImage.tsx
@@ -1,20 +1,18 @@
-import fallback from "../public/fallback-img.jpg";
-
-function MangaImage({ src, alt, fallBackSrc = fallback }) {
-  return (
-    <img
-      src={src}
-      alt={alt}
-      style={{
-        width: "100%",
-        height: "280px",
-        objectFit: "cover",
-        borderRadius: "8px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-      }}
-      onError={(e) => (e.currentTarget.src = fallBackSrc)}
-    />
-  );
+function MangaImage({ src, alt, fallBackSrc = "/fallback-img.jpg" }: { src: string; alt: string; fallBackSrc?: string }) {
+    return (
+        <img
+            src={src}
+            alt={alt}
+            style={{
+                width: "100%",
+                height: "280px",
+                objectFit: "cover",
+                borderRadius: "8px",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+            onError={(e) => (e.currentTarget.src = fallBackSrc)}
+        />
+    );
 }
 
 export default MangaImage;
