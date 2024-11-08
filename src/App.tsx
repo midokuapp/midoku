78b0fac28ed133027f59eee37ee1e62344ef80f6
Youn Mélois <youn@melois.dev>
2024-11-08 21:10:11 +0100

    fix: main overflow won't show because of wrapping div

--- src/App.tsx
+++ src/App.tsx
@@ -56,12 +56,12 @@ function Layout() {
   ];
 
   return (
-    <div>
+    <>
       <main className="flex-1 p-2 overflow-y-auto">
         <Outlet />
       </main>
       {/* Barre de navigation, shadow vers le haut */}
-      <nav className="fixed bottom-0 w-full bg-base-200 px-2 border-t border-base-100 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
+      <nav className="w-full bg-base-200 px-2 border-t border-base-100 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
         <div className="grid grid-cols-4 max-w-xl mx-auto py-3">
           {navItems.map((item) => (
             <NavLink
@@ -78,7 +78,7 @@ function Layout() {
           ))}
         </div>
       </nav>
-    </div>
+    </>
   );
 }
 
