43581fe778f78a1f793d7b8a325f086fce5cae43
Youn Mélois <youn@melois.dev>
2025-01-14 16:29:59 +0100

    feat(browse): add extension browsing template

--- src/page/browse/browse.rs
+++ src/page/browse/browse.rs
@@ -1,6 +1,32 @@
 use dioxus::prelude::*;
 
+use crate::hook::use_extensions;
+use crate::Route;
+
 #[component]
 pub fn Browse() -> Element {
-    rsx! {}
+    let extensions = use_extensions();
+
+    rsx! {
+        ul {
+            {
+                extensions
+                    .read()
+                    .iter()
+                    .map(|(_, extension)| {
+                        let extension_id = &extension.id;
+                        rsx! {
+                            li {
+                                Link {
+                                    to: Route::BrowseExtension {
+                                        extension_id: extension_id.to_string(),
+                                    },
+                                    "{extension_id}"
+                                }
+                            }
+                        }
+                    })
+            }
+        }
+    }
 }
--- src/page/browse/extension.rs
+++ src/page/browse/extension.rs
@@ -1,6 +1,15 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::icons::ld_icons::LdArrowLeft;
+use dioxus_free_icons::Icon;
 
 #[component]
 pub fn BrowseExtension(extension_id: String) -> Element {
-    rsx! {}
+    rsx! {
+        div {
+            GoBackButton {
+                Icon { style: "color: inherit", icon: LdArrowLeft }
+            }
+            h2 { "{extension_id}" }
+        }
+    }
 }
