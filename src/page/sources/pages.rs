09e93d0383bce2697ef438518d04dc88ab062c63
Youn Mélois <youn@melois.dev>
2025-01-16 16:10:11 +0100

    feat(page): list and show pages

--- src/page/sources/pages.rs
+++ src/page/sources/pages.rs
@@ -1,6 +1,43 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::icons::ld_icons::LdArrowLeft;
+use dioxus_free_icons::Icon;
+
+use crate::hook::use_state;
 
 #[component]
 pub fn PageList(extension_id: String, manga_id: String, chapter_id: String) -> Element {
-    rsx! {}
+    let state = use_state();
+    let extensions = state.extensions;
+    let extension = extensions.get(&extension_id).unwrap();
+
+    let mut page_list = use_signal(|| vec![]);
+    use_future(move || {
+        let extension = extension.clone();
+        let manga_id = manga_id.clone();
+        let chapter_id = chapter_id.clone();
+        async move { page_list.set(extension.get_page_list(manga_id, chapter_id).await.unwrap()) }
+    });
+
+    rsx! {
+        div {
+            GoBackButton {
+                Icon { style: "color: inherit", icon: LdArrowLeft }
+            }
+        }
+        ul {
+            {
+                page_list
+                    .read()
+                    .iter()
+                    .map(|page| {
+                        let page_url = page.url.clone();
+                        rsx! {
+                            li {
+                                img { src: "{page_url}" }
+                            }
+                        }
+                    })
+            }
+        }
+    }
 }
