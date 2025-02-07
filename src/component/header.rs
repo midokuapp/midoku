4a2bff4f9534c25565d6c34f743124f1c5d557f1
Youn Mélois <youn@melois.dev>
2025-02-07 22:25:18 +0100

    feat(component): make header more versatile with optional title and children

--- src/component/header.rs
+++ src/component/header.rs
@@ -3,13 +3,16 @@ use dioxus_free_icons::icons::ld_icons::LdArrowLeft;
 use dioxus_free_icons::Icon;
 
 #[component]
-pub fn Header(title: String) -> Element {
+pub fn Header(title: Option<String>, children: Element) -> Element {
     rsx! {
         div { class: "p-5 flex items-center gap-3",
             GoBackButton {
                 Icon { class: "size-6", icon: LdArrowLeft }
             }
-            h1 { class: "text-2xl font-bold", "{title}" }
+            if let Some(title) = title {
+                h1 { class: "text-2xl font-bold", "{title}" }
+            }
+            div { class: "ml-auto", {children} }
         }
     }
 }
