1c99dd2ec308345e4f6f1908b0847ba59c53cb65
Youn Mélois <youn@melois.dev>
2025-02-07 11:52:04 +0100

    feat(extensions): update extensions page using tailwindcss and show better info

--- src/page/extensions.rs
+++ src/page/extensions.rs
@@ -1,4 +1,6 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::icons::ld_icons::{LdDownload, LdLoaderCircle, LdTrash2};
+use dioxus_free_icons::Icon;
 
 use crate::hook::use_state;
 use crate::model::Manifest;
@@ -20,80 +22,147 @@ pub fn ExtensionList() -> Element {
     });
 
     rsx! {
-        input {
-            r#type: "text",
-            placeholder: "Extension repository URL",
-            value: "{repository_url}",
-            onchange: move |event| repository_url.set(event.value()),
-        }
-        h2 { "Installed" }
-        ul {
-            {
-                extensions
-                    .to_vec()
-                    .iter()
-                    .map(|extension| {
-                        let extension_id = extension.id();
-                        rsx! {
-                            li {
-                                "{extension_id}"
-                                UninstallButton { extension_id }
+        div { class: "max-w-xl mx-auto p-3",
+            h1 { class: "text-2xl font-bold mb-4", "Extension Manager" }
+            p { class: "mb-4 text-gray-700",
+                "Manage your manga extensions: Install new sources or uninstall those you no longer need."
+            }
+            input {
+                class: "w-full border border-gray-500 rounded-md mb-4 p-2",
+                r#type: "text",
+                placeholder: "Extension repository URL",
+                value: "{repository_url}",
+                onchange: move |event| repository_url.set(event.value()),
+            }
+            h2 { class: "text-xl font-semibold mb-2", "Installed" }
+            Group {
+                for extension in extensions.to_vec().iter() {
+                    Item {
+                        ItemIcon {
+                            src: extension.icon_path().to_string_lossy().to_string(),
+                            alt: extension.source().name.clone(),
+                        }
+                        ItemDetail {
+                            ItemTitle { title: extension.source().name.clone() }
+                            ItemDescription {
+                                language: extension.source().language.clone(),
+                                version: extension.source().version.clone(),
+                                nsfw: extension.source().nsfw,
                             }
                         }
-                    })
+                        UninstallButton { extension_id: extension.id() }
+                    }
+                }
             }
-        }
-        h2 { "Available" }
-        ul {
-            {
-                manifests
-                    .read()
-                    .iter()
-                    .flat_map(|manifest| {
-                        let extension_id = &manifest.id;
-                        (!extensions.contains(extension_id))
-                            .then(|| rsx! {
-                                li {
-                                    "{extension_id}"
-                                    InstallButton { manifest: manifest.clone() }
-                                }
-                            })
-                    })
+            h2 { class: "text-xl font-semibold mb-2", "Available" }
+            Group {
+                for manifest in manifests().iter().filter(|manifest| !extensions.contains(&manifest.id)) {
+                    Item {
+                        ItemIcon {
+                            src: "{repository_url}/icons/{manifest.icon}",
+                            alt: manifest.name.clone(),
+                        }
+                        ItemDetail {
+                            ItemTitle { title: manifest.name.clone() }
+                            ItemDescription {
+                                language: manifest.language.clone(),
+                                version: manifest.version.clone(),
+                                nsfw: manifest.nsfw,
+                            }
+                        }
+                        InstallButton { manifest: manifest.clone() }
+                    }
+                }
             }
         }
     }
 }
 
 #[component]
-pub fn InstallButton(manifest: Manifest) -> Element {
+fn Group(children: Element) -> Element {
+    rsx! {
+        ul { class: "space-y-4 mb-8", {children} }
+    }
+}
+
+#[component]
+fn Item(children: Element) -> Element {
+    rsx! {
+        li { class: "flex item-center gap-4 p-3 rounded-lg shadow-md", {children} }
+    }
+}
+
+#[component]
+fn ItemIcon(src: String, alt: String) -> Element {
+    rsx! {
+        figure { class: "size-12",
+            img { class: "rounded-md", src, alt }
+        }
+    }
+}
+
+#[component]
+fn ItemDetail(children: Element) -> Element {
+    rsx! {
+        div { class: "flex flex-col", {children} }
+    }
+}
+
+#[component]
+fn ItemTitle(title: String) -> Element {
+    rsx! {
+        h3 { class: "text-lg font-semibold", "{title}" }
+    }
+}
+
+#[component]
+fn ItemDescription(language: String, version: String, nsfw: bool) -> Element {
+    rsx! {
+        p { class: "text-sm",
+            span { class: "text-gray-700", "{language} {version}" }
+            if nsfw {
+                span { class: "text-red-500", " +18" }
+            }
+        }
+    }
+}
+
+#[component]
+fn InstallButton(manifest: Manifest) -> Element {
     let mut state = use_state();
 
     let mut disabled = use_signal(|| false);
 
     rsx! {
         button {
+            class: "ml-auto rounded-full bg-gray-100 hover:bg-green-500 size-12 flex justify-center items-center",
             disabled: "{disabled}",
             onclick: move |_| {
                 disabled.set(true);
                 let manifest = manifest.clone();
                 async move { state.install_extension(&manifest).await.unwrap() }
             },
-            "Install"
+            if disabled() {
+                Icon { class: "size-5 animate-spin", icon: LdLoaderCircle }
+            } else {
+                Icon { class: "size-5", icon: LdDownload }
+            }
         }
     }
 }
 
 #[component]
-pub fn UninstallButton(extension_id: String) -> Element {
+fn UninstallButton(extension_id: String) -> Element {
     let mut state = use_state();
 
     rsx! {
         button {
+            class: "ml-auto rounded-full bg-gray-100 hover:bg-red-500 size-12 flex justify-center items-center",
             onclick: move |_| {
                 let extension_id = extension_id.clone();
                 async move { state.uninstall_extension(&extension_id).await.unwrap() }
             },
-            "Uninstall"
+            Icon { class: "size-5", icon: LdTrash2 }
         }
     }
 }
