a601e45cd909467c4019db967cd5be0708fa63a0
Youn Mélois <youn@melois.dev>
2025-02-07 17:02:15 +0100

    fix(mangas): stretching grid

--- src/page/sources/mangas.rs
+++ src/page/sources/mangas.rs
@@ -104,8 +104,10 @@ fn Header(title: String) -> Element {
 #[component]
 fn Grid(children: Element) -> Element {
     rsx! {
-        ul { class: "p-2 flex-1 overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(100px,5fr))] gap-3",
-            {children}
+        div { class: "flex-1",
+            ul { class: "p-2 overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(100px,5fr))] gap-3",
+                {children}
+            }
         }
     }
 }
