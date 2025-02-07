92edb5a2b8a45438f885ee4706e2c4de2dd240f5
Youn Mélois <youn@melois.dev>
2025-02-07 17:08:04 +0100

    fix(mangas): moved overflow on ul's wrapper

--- src/page/sources/mangas.rs
+++ src/page/sources/mangas.rs
@@ -104,8 +104,8 @@ fn Header(title: String) -> Element {
 #[component]
 fn Grid(children: Element) -> Element {
     rsx! {
-        div { class: "flex-1",
-            ul { class: "p-2 overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(100px,5fr))] gap-3",
+        div { class: "flex-1 overflow-y-auto",
+            ul { class: "p-2 grid grid-cols-[repeat(auto-fill,minmax(100px,5fr))] gap-3",
                 {children}
             }
         }
