2f5fa46ba358fc5ac683bb79d4ecf4c0e3a44ba1
Youn Mélois <youn@melois.dev>
2025-02-07 14:34:59 +0100

    feat(mangas): show mangas as grid and show cover

--- src/page/sources/mangas.rs
+++ src/page/sources/mangas.rs
@@ -15,7 +15,7 @@ pub fn MangaState() -> Element {
     });
 
     rsx! {
-        Outlet::<Route> {}
+        main { class: "flex flex-col h-screen", Outlet::<Route> {} }
     }
 }
 
@@ -58,25 +58,21 @@ pub fn MangaList(extension_id: String) -> Element {
     });
 
     rsx! {
-        div {
-            GoBackButton {
-                Icon { style: "color: inherit", icon: LdArrowLeft }
-            }
-            h2 { "{name}" }
-        }
-        ul { id: "manga-grid",
+        Header { title: name.clone() }
+        Grid {
             for manga in self_state.mangas.read().iter() {
-                li {
-                    Link {
-                        to: Route::ChapterList {
-                            extension_id: extension_id.clone(),
-                            manga_id: manga.id.clone(),
-                        },
-                        "{manga.title}"
+                Item {
+                    extension_id: extension_id.clone(),
+                    manga_id: manga.id.clone(),
+                    ItemImage {
+                        src: manga.cover_url.clone(),
+                        alt: manga.title.clone(),
                     }
+                    ItemTitle { title: manga.title.clone() }
                 }
             }
-            div {
+            li {
+                class: "col-span-full",
                 onvisible: move |event| {
                     let data = event.data();
                     let is_intersecting = data.is_intersecting().unwrap_or_default();
@@ -87,5 +83,61 @@ pub fn MangaList(extension_id: String) -> Element {
                 "loading..."
             }
         }
+    }
+}
+
+#[component]
+fn Header(title: String) -> Element {
+    rsx! {
+        div { class: "p-5 flex items-center gap-3",
+            GoBackButton {
+                Icon { style: "color: inherit", icon: LdArrowLeft }
+            }
+            h1 { class: "text-2xl font-bold", "{title}" }
+        }
+    }
+}
+
+#[component]
+fn Grid(children: Element) -> Element {
+    rsx! {
+        ul { class: "p-2 flex-1 overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(100px,5fr))] gap-3",
+            {children}
+        }
+    }
+}
+
+#[component]
+fn Item(extension_id: String, manga_id: String, children: Element) -> Element {
+    rsx! {
+        li {
+            Link {
+                to: Route::ChapterList {
+                    extension_id,
+                    manga_id,
+                },
+                {children}
+            }
+        }
+    }
+}
+
+#[component]
+fn ItemImage(src: String, alt: String) -> Element {
+    rsx! {
+        figure { class: "w-full aspect-[2/3]",
+            img {
+                class: "w-full h-full object-cover rounded-md",
+                src,
+                alt,
+            }
+        }
+    }
+}
+
+#[component]
+fn ItemTitle(title: String) -> Element {
+    rsx! {
+        p { class: "mx-1 mt-1 line-clamp-2 text-sm font-bold", "{title}" }
     }
 }
